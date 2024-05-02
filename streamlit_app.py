3dd59355cbe821bfdc63e0444704344fb324c04d
jkrishna12 <152908825+jkrishna12@users.noreply.github.com>
2024-05-02 08:51:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ ingredient_list = st.multiselect(
     )
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 if ingredient_list:
     
